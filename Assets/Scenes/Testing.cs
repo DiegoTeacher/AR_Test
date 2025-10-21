eadc502a11b42e6601d8280e7dd5915e62f4119d
DiegoTeacher <diego.baratto@ifp.es>
2025-10-21 09:46:13 +0200

    Update Testing.cs

--- Assets/Scenes/Testing.cs
+++ Assets/Scenes/Testing.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-    public int var1;
+    public int var1, otrasss;
     // Start is called before the first frame update
     void Start()
     {
